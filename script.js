14ab7b27a87c91326229b21b414feaa2e753fdc0
UnknowAi123 <keiro32@gmail.com>
2025-01-16 03:50:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ window.onload = function() {
         ctx.fillText(currentCard.type, 220, 130);
 
         //Title
-        let titleFontSize = currentCard.title.length >= 26 ? 14
+        let titleFontSize = currentCard.title.length >= 27 ? 14
             : currentCard.title.length >= 17 ? 16 : 24;
         ctx.textAlign = "center";
         ctx.font = 'bold ' + titleFontSize + 'px Merriweather';
